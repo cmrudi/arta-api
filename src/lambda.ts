4315a339ea756315d23724c334d127c2604b0b52
Cut Meurah Rudi <cutmeurahrudi@CLSGLPM260004.local>
2026-03-04 02:39:29 +0800

    add logs

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,20 @@
 import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context } from 'aws-lambda';
 
 import app from './app';
 
-export const handler = serverlessExpress({ app });
+const serverlessHandler = serverlessExpress({ app });
+
+export const handler = async (event: unknown, context: Context): Promise<unknown> =>
+	new Promise((resolve, reject) => {
+		const callback: Callback<unknown> = (error, result) => {
+			if (error) {
+				reject(error);
+				return;
+			}
+
+			resolve(result);
+		};
+
+		serverlessHandler(event, context, callback);
+	});
